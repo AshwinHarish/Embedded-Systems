674e9d8c5a74760b0b991a3746d71734a17d354c
Ashwin Harish <ashwin645@gmail.com>
2019-05-28 15:54:12 +0530

    Update Question_1a.c

--- Question_1a.c
+++ Question_1a.c
@@ -10,16 +10,16 @@ int32_t main (void)
     uint16_t value;  
     char TEXT[16];			  
     UNLOCKREG();
-	SYSCLK->PWRCON.XTL12M_EN = 1; 	//Enable 12Mhz and set HCLK->12Mhz
-	SYSCLK->CLKSEL0.HCLK_S = 0;
-	LOCKREG();
-	Initial_panel();  // initialize LCD panel
+    SYSCLK->PWRCON.XTL12M_EN = 1; 	//Enable 12Mhz and set HCLK->12Mhz
+    SYSCLK->CLKSEL0.HCLK_S = 0;
+    LOCKREG();
+    Initial_panel();  // initialize LCD panel
     clr_all_panel();  // clear LCD panel 
     print_lcd(0,"variable resistor");
 
-  	DrvADC_Open(ADC_SINGLE_END,ADC_SINGLE_OP , 0x40,INTERNAL_HCLK , 1); 	
+    DrvADC_Open(ADC_SINGLE_END,ADC_SINGLE_OP , 0x40,INTERNAL_HCLK , 1); 	
   	
-	while(1)
+    while(1)
     {
         DrvADC_StartConvert();   // start A/D conversion
         while(DrvADC_IsConversionDone()==FALSE);
