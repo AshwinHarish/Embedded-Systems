0c387ecd8a082a2c5265b9c904846b2f3a18f4e0
Ashwin Harish <ashwin645@gmail.com>
2019-05-28 14:27:22 +0530

    Add files via upload

--- Question_1a.c
+++ Question_1a.c
@@ -9,12 +9,11 @@ int32_t main (void)
 {  
     uint16_t value;  
     char TEXT[16];			  
-	
     UNLOCKREG();
-	  SYSCLK->PWRCON.XTL12M_EN = 1; 	//Enable 12Mhz and set HCLK->12Mhz
-	  SYSCLK->CLKSEL0.HCLK_S = 0;
-	  LOCKREG();
-	  Initial_panel();  // initialize LCD panel
+	SYSCLK->PWRCON.XTL12M_EN = 1; 	//Enable 12Mhz and set HCLK->12Mhz
+	SYSCLK->CLKSEL0.HCLK_S = 0;
+	LOCKREG();
+	Initial_panel();  // initialize LCD panel
     clr_all_panel();  // clear LCD panel 
     print_lcd(0,"variable resistor");
 
